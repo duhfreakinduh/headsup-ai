1abdfc7fc723567abffb87e8f6112d32d45ad758
duhfreakinduh <duhfreakinduh@gmail.com>
2026-08-02 11:01:31 -0500

    Add offline service worker

---
+++ family-day-planner/sw.js
@@ -0,0 +1,29 @@
+const CACHE = "family-day-builder-v1";
+const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(match => match || caches.match("./index.html")))
+  );
+});
