a054e9f61f837eff29e428343049911bb1856e31
duhfreakinduh <duhfreakinduh@gmail.com>
2026-08-22 08:03:32 -0500

    Bump Android app to 1.6 sensitivity build

--- driver-guard-android/app/build.gradle.kts
+++ driver-guard-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.duhfreakinduh.driverguard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5-native"
+        versionCode = 7
+        versionName = "1.6-native"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
