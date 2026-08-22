8cec9fa4769767a00e5504d42ff4de5a462c3040
duhfreakinduh <duhfreakinduh@gmail.com>
2026-08-22 07:24:06 -0500

    Bump Android app to v1.5 Smith Road Guard

--- driver-guard-android/app/build.gradle.kts
+++ driver-guard-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.duhfreakinduh.driverguard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4-native"
+        versionCode = 6
+        versionName = "1.5-native"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
